d106a2306de9b5d5253b3dec3da0f3db1a73f0a7
Abdullah Ayman <84142402+AbdullahKhkmba@users.noreply.github.com>
2026-04-26 18:58:43 +0300

    add three more card ids for testing + minimize room 1 capacity to 5 for testing room completion

--- state.py
+++ state.py
@@ -2,18 +2,21 @@ sessions = dict()
 rooms = {'R1': {
                     'id': 'R1',
                     'name': 'Room 1',
-                    'total_capacity': 100,
+                    'total_capacity': 5,
                 },
         'R2': {
                     'id': 'R2',
                     'name': 'Room 2',
-                    'total_capacity': 200,
+                    'total_capacity': 10,
                 }
         }
 scans = dict()
 cards = {
             '5D03B302':'Abdullah',
-            '51BBB802':'Mouhamed'
+            '51BBB802':'Shady',
+            'A43BC906':'Dr.Ali',
+            '669FC906':'Abdelmohsen',
+            'D4FFC806':'Zeyad'
         }
 
 active_session_id = None
